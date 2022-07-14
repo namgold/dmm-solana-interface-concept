063a496d0de6930e3e6faa79b64726106c32a9d7
Nam Nguyen <nam.nguyen@kyber.network>
2022-07-15 00:36:18 +0700

    add ignore vite dist
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,9 +11,9 @@ export default defineConfig({
   },
   define: {
     'process.env': {},
-    'global': {},
+    global: {},
   },
   server: {
-    port: 4000
-  }
-});
+    port: 4000,
+  },
+})
